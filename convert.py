b6c0f1fd7a5febc9731f6dc319d40338ae06ab09
ToNy <73397829+arsenalon14@users.noreply.github.com>
2023-07-16 13:41:55 +0700

    Update convert.py
    Add continue to keep asking if not input 1 or 2. And add Break when run complete

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
+## update code to make it ask for number again if input wrong number.
 ## make convert program that convert from/to celsius to/from fahrenheit.
 
 ## Convert Fahrenheit to Celsius.
@@ -12,21 +13,25 @@ print("Hi do you like to convert for celsius to fahrenheit or fahrenheit to cels
 print("Type 1 for Celsius to fahrenheit")
 print("Type 2 for fahrenheit ot celsius")
 
-
-while True:
+answer =""
+complete = False
+while not complete:
 ## input only 1 or 2
     answer = input()
-    if answer in ("1"):
+    if answer == "1":
         num1 = float(input("Please insert number in Celsius: "))
         result1 = far(num1)
         print(f"From {num1} Celsius is {result1:.2f} Fahrenheit")
-    elif answer in ("2"):
+        complete = True
+    elif answer == "2":
         num2 = float(input("Please insert number in Fahrenheit: "))
         result2 = cel(num2)
         print(f"From {num2} Fahrenheit is {result2:.2f} Celsius")
-    else:
-        print("Wrong number")
-    break
-
+        complete = True
+    else: ## If input other number
+        print("Wrong number\nPlease in put number again\nType 1 for Celsius to fahrenheit\nType 2 for fahrenheit ot celsius)
+    continue
+    
+print("Exiting program")
 
 
